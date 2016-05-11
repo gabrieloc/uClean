172ec9cba5fb29707cd34914257dc16a80206199
Gabriel O'Flaherty-Chan <gabriel@shopify.com>
2016-05-11 16:40:08 +0900

    clean up liftable behaviour

--- Assets/scripts/PropController.cs
+++ Assets/scripts/PropController.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class PropController : MonoBehaviour 
 {
-	public int initialSpawn = 1;
+	public int initialSpawn = 10;
+	public float displacement = 2.0f;
 
 	void Start()
 	{
 		for (int index = 0; index < initialSpawn; index++) {
 			GameObject prop = CleanKit.CreateTestProp();
 			prop.transform.SetParent(transform, false);
-			prop.transform.position = new Vector3(0, 2, 2);
+			prop.transform.position = new Vector3((Random.value + 1) * displacement * (Random.value > 0.5 ? 1 : -1), 5, (Random.value + 1) * displacement * (Random.value > 0.5 ? 1 : -1));
+			Debug.Log(prop.transform.position);
 		}
 	}
 
