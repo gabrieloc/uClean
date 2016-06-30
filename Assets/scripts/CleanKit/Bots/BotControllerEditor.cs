77f1208c842335a22c44a801791809d41fe38c37
Gabriel O'Flaherty-Chan <gabriel@shopify.com>
2016-06-29 23:34:21 -0400

    remove add bot button from gui

---
+++ Assets/scripts/CleanKit/Bots/BotControllerEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace CleanKit
+{
+	[CustomEditor (typeof(BotController))]
+
+	public class BotControllerEditor : Editor
+	{
+		public override void OnInspectorGUI ()
+		{
+			DrawDefaultInspector ();
+			BotController botController = (BotController)target;
+			if (GUILayout.Button ("Add Bot")) {
+				botController.AddBot ();			
+			}
+		}
+	}
+}
