4cf94e90c128a93b943ac56dd7ac6ebf47f90af5
Gabriel O'Flaherty-Chan <gabriel@shopify.com>
2016-07-26 00:59:01 -0400

    Add static assets

---
+++ Assets/scripts/CleanKit/Game/AppCoordinator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace CleanKit
+{
+	public class AppCoordinator : MonoBehaviour
+	{
+		enum Scene
+		{
+			Launch = 1,
+			Setup,
+			Room
+		}
+
+		public void RouteToSetup ()
+		{
+			routeToScene (Scene.Setup);
+		}
+
+		public void RouteToRoom ()
+		{
+			routeToScene (Scene.Room);
+		}
+
+		void routeToScene (Scene scene)
+		{
+			SceneManager.LoadScene (identifierForScene (scene), LoadSceneMode.Additive);
+		}
+
+		string identifierForScene (Scene scene)
+		{
+			switch (scene) {
+			case Scene.Launch:
+				return "launch";
+			case Scene.Setup:
+				return "setup";
+			case Scene.Room:
+				return "room";
+			default:
+				return null;
+			}
+		}
+	}
+}
